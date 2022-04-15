c70b994375f71e620b6380c9938aa3f31fb435ec
uvsq22102840 <marwa.fidah-mouro@ens.uvsq.fr>
2022-04-15 09:21:38 +0200

    modif

--- projet_taquin.py
+++ projet_taquin.py
@@ -9,3 +9,8 @@
 ######################################################
 
 from tkinter import Tk 
+from random import randint 
+
+def taquin():
+    grille= [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]]
+
